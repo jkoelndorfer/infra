ba0c618c49d57ac456da6bf513d09c46bf13e703
John Koelndorfer <john@johnk.io>
2021-09-01 18:11:24 -0500

    terraform: Rename 0500-blog to 0500-website

--- terraform/stacks/0500-website/backend.tf
+++ terraform/stacks/0500-website/backend.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket               = "310987624463-tfstate-prod"
-    key                  = "blog.tfstate"
+    key                  = "website.tfstate"
     region               = "us-east-1"
     workspace_key_prefix = "workspaces"
   }
