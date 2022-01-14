c07b402bdd9a3aaa80be954e8fbe9ce99dfc266f
John Koelndorfer <john@johnk.io>
2022-01-14 13:37:26 -0600

    website_persistent: Add additional assets bucket
    These assets will be copied to the S3 bucket hosting my website at
    deploy time. Useful for things that are not to be committed to a git
    repository.

--- terraform/stacks/0400-website_persistent/main.tf
+++ terraform/stacks/0400-website_persistent/main.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "website_bucket" {
     error_document = "404.html"
   }
 }
+
+# This bucket contains additional assets deployed to the johnk.io website
+# which are not committed to the website repository.
+resource "aws_s3_bucket" "website_addl_assets_bucket" {
+  bucket = "${local.aws_account_id}-website-addl-assets-${local.env.name}"
+  acl    = "private"
+}
