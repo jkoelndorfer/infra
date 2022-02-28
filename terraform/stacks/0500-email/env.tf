518ed96897abaf9f3bcb81b920197591b92e81df
John Koelndorfer <john@johnk.io>
2022-02-28 13:54:47 -0600

    email: Migrate to ProtonMail

--- terraform/stacks/0500-email/env.tf
+++ terraform/stacks/0500-email/env.tf
@@ -1,11 +1,22 @@
 locals {
   environments = {
     prod = {
-      aws_mail_region = "us-east-1"
       default_dns_ttl = 600
-      spf_record      = "v=spf1 include:amazonses.com ~all"
-      dmarc_record    = "v=DMARC1;p=quarantine;pct=100;fo=1"
-      dns_zone        = "johnk.io"
+
+      ownership_verification = "protonmail-verification=c0b1cbcaa68e509c32ea4c4e85d01dae3b491b1e"
+      spf_record             = "v=spf1 include:_spf.protonmail.ch mx ~all"
+
+      dkim_records = {
+        protonmail  = "protonmail.domainkey.dbdts5dg2px7ags5zzewhcfpqelug5qq35btpwbnil42vmy6ipsza.domains.proton.ch."
+        protonmail2 = "protonmail2.domainkey.dbdts5dg2px7ags5zzewhcfpqelug5qq35btpwbnil42vmy6ipsza.domains.proton.ch."
+        protonmail3 = "protonmail3.domainkey.dbdts5dg2px7ags5zzewhcfpqelug5qq35btpwbnil42vmy6ipsza.domains.proton.ch."
+      }
+      mx_records = [
+        "10 mail.protonmail.ch",
+        "20 mailsec.protonmail.ch",
+      ]
+      dmarc_record = "v=DMARC1;p=quarantine;pct=100;fo=1"
+      dns_zone     = "johnk.io"
     }
   }
 }
--- terraform/stacks/0500-email/main.tf
+++ terraform/stacks/0500-email/main.tf
@@ -4,39 +4,12 @@ data "aws_route53_zone" "email_domain" {
   private_zone = false
 }
 
-resource "aws_ses_domain_identity" "email_domain" {
-  domain = local.env.dns_zone
-}
-
-resource "aws_ses_domain_mail_from" "email_mail_from" {
-  domain           = aws_ses_domain_identity.email_domain.domain
-  mail_from_domain = "mail.${aws_ses_domain_identity.email_domain.domain}"
-}
-
-resource "aws_ses_domain_dkim" "email_dkim" {
-  domain = aws_ses_domain_identity.email_domain.domain
-}
-
-resource "aws_route53_record" "ses_verification" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "_amazonses.${local.env.dns_zone}"
-  type    = "TXT"
-  ttl     = local.env.default_dns_ttl
-  records = [aws_ses_domain_identity.email_domain.verification_token]
-}
-
-resource "aws_ses_domain_identity_verification" "email_domain" {
-  domain = aws_ses_domain_identity.email_domain.id
-
-  depends_on = [aws_route53_record.ses_verification]
-}
-
-resource "aws_route53_record" "spf" {
+resource "aws_route53_record" "ownership_verification_and_spf" {
   zone_id = data.aws_route53_zone.email_domain.zone_id
   name    = local.env.dns_zone
   type    = "TXT"
   ttl     = local.env.default_dns_ttl
-  records = [local.env.spf_record]
+  records = [local.env.ownership_verification, local.env.spf_record]
 }
 
 resource "aws_route53_record" "dmarc_record" {
@@ -52,15 +25,7 @@ resource "aws_route53_record" "mx" {
   name    = local.env.dns_zone
   type    = "MX"
   ttl     = local.env.default_dns_ttl
-  records = ["10 inbound-smtp.${local.env.aws_mail_region}.amazonaws.com."]
-}
-
-resource "aws_route53_record" "autodiscover" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "autodiscover.${local.env.dns_zone}"
-  type    = "CNAME"
-  ttl     = local.env.default_dns_ttl
-  records = ["autodiscover.mail.${local.env.aws_mail_region}.awsapps.com."]
+  records = local.env.mx_records
 }
 
 resource "aws_route53_record" "mail_mx" {
@@ -68,7 +33,7 @@ resource "aws_route53_record" "mail_mx" {
   name    = "mail.${local.env.dns_zone}"
   type    = "MX"
   ttl     = local.env.default_dns_ttl
-  records = ["10 feedback-smtp.${local.env.aws_mail_region}.amazonses.com."]
+  records = local.env.mx_records
 }
 
 resource "aws_route53_record" "mail_spf" {
@@ -80,11 +45,11 @@ resource "aws_route53_record" "mail_spf" {
 }
 
 resource "aws_route53_record" "dkim" {
-  count = 3
+  for_each = local.env.dkim_records
 
   zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "${element(aws_ses_domain_dkim.email_dkim.dkim_tokens, count.index)}._domainkey.${local.env.dns_zone}."
+  name    = "${each.key}._domainkey.${local.env.dns_zone}."
   type    = "CNAME"
   ttl     = local.env.default_dns_ttl
-  records = ["${element(aws_ses_domain_dkim.email_dkim.dkim_tokens, count.index)}.dkim.amazonses.com."]
+  records = [each.value]
 }
