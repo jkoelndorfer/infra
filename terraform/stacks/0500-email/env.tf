a47b60fdc7696c8b1084c599c2a2d5487b8f5950
John Koelndorfer <john@johnk.io>
2022-08-21 10:51:55 -0500

    terraform/0500-email: Configure johnk.io email with email module

--- terraform/stacks/0500-email/env.tf
+++ terraform/stacks/0500-email/env.tf
@@ -1,21 +1,24 @@
 locals {
   environments = {
     prod = {
-      default_dns_ttl = 600
+      dns_ttl             = 600
+      dmarc_record        = "v=DMARC1;p=quarantine;pct=100;fo=1"
+      fastmail_spf_record = "v=spf1 include:spf.messagingengine.com ?all"
 
-      spf_record = "v=spf1 include:spf.messagingengine.com ?all"
+      johnk_io = {
+        dkim_records = {
+          fm1 = "fm1.johnk.io.dkim.fmhosted.com"
+          fm2 = "fm2.johnk.io.dkim.fmhosted.com"
+          fm3 = "fm3.johnk.io.dkim.fmhosted.com"
+        }
 
-      dkim_records = {
-        fm1 = "fm1.johnk.io.dkim.fmhosted.com"
-        fm2 = "fm2.johnk.io.dkim.fmhosted.com"
-        fm3 = "fm3.johnk.io.dkim.fmhosted.com"
+        mx_records = [
+          "10 in1-smtp.messagingengine.com",
+          "20 in2-smtp.messagingengine.com",
+        ]
+
+        zone = "johnk.io"
       }
-      mx_records = [
-        "10 in1-smtp.messagingengine.com",
-        "20 in2-smtp.messagingengine.com",
-      ]
-      dmarc_record = "v=DMARC1;p=quarantine;pct=100;fo=1"
-      dns_zone     = "johnk.io"
     }
   }
 }
--- terraform/stacks/0500-email/main.tf
+++ terraform/stacks/0500-email/main.tf
@@ -1,55 +1,11 @@
-# NOTE: This stack depends on AWS WorkMail, for which there is not a Terraform resource.
-data "aws_route53_zone" "email_domain" {
-  name         = local.env.dns_zone
-  private_zone = false
-}
+module "johnk_io" {
+  source = "../../modules/email-zone/v1"
 
-resource "aws_route53_record" "spf" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = local.env.dns_zone
-  type    = "TXT"
-  ttl     = local.env.default_dns_ttl
-  records = [local.env.spf_record]
-}
+  zone = local.env.johnk_io.zone
 
-resource "aws_route53_record" "dmarc_record" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "_dmarc.${local.env.dns_zone}"
-  type    = "TXT"
-  ttl     = local.env.default_dns_ttl
-  records = [local.env.dmarc_record]
-}
-
-resource "aws_route53_record" "mx" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = local.env.dns_zone
-  type    = "MX"
-  ttl     = local.env.default_dns_ttl
-  records = local.env.mx_records
-}
-
-resource "aws_route53_record" "mail_mx" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "mail.${local.env.dns_zone}"
-  type    = "MX"
-  ttl     = local.env.default_dns_ttl
-  records = local.env.mx_records
-}
-
-resource "aws_route53_record" "mail_spf" {
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "mail.${local.env.dns_zone}"
-  type    = "TXT"
-  ttl     = local.env.default_dns_ttl
-  records = [local.env.spf_record]
-}
-
-resource "aws_route53_record" "dkim" {
-  for_each = local.env.dkim_records
-
-  zone_id = data.aws_route53_zone.email_domain.zone_id
-  name    = "${each.key}._domainkey.${local.env.dns_zone}."
-  type    = "CNAME"
-  ttl     = local.env.default_dns_ttl
-  records = [each.value]
+  dmarc_record = local.env.dmarc_record
+  dkim_records = local.env.johnk_io.dkim_records
+  dns_ttl      = local.env.dns_ttl
+  mx_records   = local.env.johnk_io.mx_records
+  spf_record   = local.env.fastmail_spf_record
 }
