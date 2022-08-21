4cffc0132ba123bbd094e6ed1cf5e59952bea767
John Koelndorfer <john@johnk.io>
2022-08-21 11:02:08 -0500

    terraform/0500-email: Point koelndorfer.com email at Fastmail

--- terraform/stacks/0500-email/env.tf
+++ terraform/stacks/0500-email/env.tf
@@ -5,6 +5,29 @@ locals {
       dmarc_record        = "v=DMARC1;p=quarantine;pct=100;fo=1"
       fastmail_spf_record = "v=spf1 include:spf.messagingengine.com ?all"
 
+      koelndorfer_com = {
+        dkim_records = {
+          fm1 = "fm1.koelndorfer.com.dkim.fmhosted.com"
+          fm2 = "fm2.koelndorfer.com.dkim.fmhosted.com"
+          fm3 = "fm3.koelndorfer.com.dkim.fmhosted.com"
+        }
+
+        # Previously, koelndorfer.com had MX records pointing
+        # at GMail:
+        #
+        # 10 alt2.aspmx.l.google.com.
+        # 10 aspmx2.googlemail.com.
+        # 10 alt1.aspmx.l.google.com.
+        # 10 aspmx.l.google.com.
+        # 10 aspmx3.googlemail.com.
+        mx_records = [
+          "10 in1-smtp.messagingengine.com",
+          "20 in2-smtp.messagingengine.com",
+        ]
+
+        zone = "koelndorfer.com"
+      }
+
       johnk_io = {
         dkim_records = {
           fm1 = "fm1.johnk.io.dkim.fmhosted.com"
--- terraform/stacks/0500-email/main.tf
+++ terraform/stacks/0500-email/main.tf
@@ -9,3 +9,15 @@ module "johnk_io" {
   mx_records   = local.env.johnk_io.mx_records
   spf_record   = local.env.fastmail_spf_record
 }
+
+module "koelndorfer_com" {
+  source = "../../modules/email-zone/v1"
+
+  zone = local.env.koelndorfer_com.zone
+
+  dmarc_record = local.env.dmarc_record
+  dkim_records = local.env.koelndorfer_com.dkim_records
+  dns_ttl      = local.env.dns_ttl
+  mx_records   = local.env.koelndorfer_com.mx_records
+  spf_record   = local.env.fastmail_spf_record
+}
