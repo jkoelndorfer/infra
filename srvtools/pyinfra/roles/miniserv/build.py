75a01b1e66bf697064c3e12d9c6e8ecec29bf6f2
John Koelndorfer <john@johnk.io>
2023-05-29 10:28:41 -0500

    pyinfra: Install aqgo on miniserv

--- srvtools/pyinfra/roles/miniserv/build.py
+++ srvtools/pyinfra/roles/miniserv/build.py
@@ -10,16 +10,35 @@ from os import path
 from pyinfra.operations import apt, files
 
 from lib import vars as gvars
-from lib.build.common import build_common
+from lib.aws import aws_access_key_id, aws_secret_access_key
+from lib.build.common import build_common, install_package_set
 from lib.build.docker import install_docker
+from lib.operations import aws
 
 from . import vars
 
 
 def build():
     build_common()
+    install_package_set("aws-cli")
+    install_aqgo()
     install_docker()
     install_rclone()
+
+
+def install_aqgo():
+    aws.s3_download(
+        name="[aqgo] download aqgo binary",
+        src=vars.aqgo_s3_uri,
+        dest="/usr/local/bin/aqgo",
+        user="root",
+        group="root",
+        mode="0555",
+        sha256sum=vars.aqgo_sha256sum,
+        aws_access_key_id=aws_access_key_id(),
+        aws_secret_access_key=aws_secret_access_key(),
+        _sudo=True,
+    )
 
 
 def install_rclone():
