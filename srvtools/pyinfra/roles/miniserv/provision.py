94c2d650995788bb5966dd9dab50bb6d8742e1cd
John Koelndorfer <john@johnk.io>
2023-08-31 12:58:02 -0500

    miniserv: Don't "with" to set pyinfra context

--- srvtools/pyinfra/roles/miniserv/provision.py
+++ srvtools/pyinfra/roles/miniserv/provision.py
@@ -28,40 +28,40 @@ def provision(p: Pyinfra):
 
 def provision_aqgo(pyinfra: Pyinfra):
     aqgo_files_dir = path.join(vars.files_dir, "aqgo")
+    p = pyinfra.ctx("aqgo")
 
-    with pyinfra.ctx("aqgo") as p:
-        aqgo_env = p.files.template(
-            name="env",
-            src=path.join(aqgo_files_dir, "aqgo.conf.j2"),
-            dest="/etc/aqgo.conf",
-            user="root",
-            group="root",
-            mode="0400",
-            quote=quote,
-            aws_default_region=gvars.aws_default_region,
-            aws_access_key_id=ssm_parameter_value("/prod/air_quality/aws_iam_access_key_id"),
-            aws_secret_access_key=ssm_parameter_value("/prod/air_quality/aws_iam_secret_access_key"),
-        )
+    aqgo_env = p.files.template(
+        name="env",
+        src=path.join(aqgo_files_dir, "aqgo.conf.j2"),
+        dest="/etc/aqgo.conf",
+        user="root",
+        group="root",
+        mode="0400",
+        quote=quote,
+        aws_default_region=gvars.aws_default_region,
+        aws_access_key_id=ssm_parameter_value("/prod/air_quality/aws_iam_access_key_id"),
+        aws_secret_access_key=ssm_parameter_value("/prod/air_quality/aws_iam_secret_access_key"),
+    )
 
-        aqgo_systemd_unit = p.files.template(
-            name="service systemd unit",
-            src=path.join(aqgo_files_dir, "aqgo.service.j2"),
-            dest=path.join(gvars.systemd_unit_dir, "aqgo.service"),
-            user="root",
-            group="root",
-            mode="0444",
-            serial_device_path=vars.aqgo_serial_device_path,
-            metric_namespace=vars.aqgo_metric_namespace,
-        )
+    aqgo_systemd_unit = p.files.template(
+        name="service systemd unit",
+        src=path.join(aqgo_files_dir, "aqgo.service.j2"),
+        dest=path.join(gvars.systemd_unit_dir, "aqgo.service"),
+        user="root",
+        group="root",
+        mode="0444",
+        serial_device_path=vars.aqgo_serial_device_path,
+        metric_namespace=vars.aqgo_metric_namespace,
+    )
 
-        p.systemd.service(
-            name="start aqgo",
-            service="backup.timer",
-            running=True,
-            restarted=aqgo_env.changed or aqgo_systemd_unit.changed,
-            enabled=True,
-            daemon_reload=aqgo_systemd_unit.changed,
-        )
+    p.systemd.service(
+        name="start aqgo",
+        service="backup.timer",
+        running=True,
+        restarted=aqgo_env.changed or aqgo_systemd_unit.changed,
+        enabled=True,
+        daemon_reload=aqgo_systemd_unit.changed,
+    )
 
 
 def provision_container_networks(pyinfra: Pyinfra):
@@ -72,79 +72,79 @@ def provision_container_networks(pyinfra: Pyinfra):
 def provision_rclone_backup(pyinfra: Pyinfra):
     backup_files_dir = path.join(vars.files_dir, "backup")
     backup_config_dir = "/etc/backup"
+    p = pyinfra.ctx("rclone")
 
-    with pyinfra.ctx("rclone") as p:
-        p.files.put(
-            name="install backup script",
-            src=path.join(backup_files_dir, "backup"),
-            dest="/usr/local/bin/backup",
-            user="root",
-            group="root",
-            mode="0555",
-        )
+    p.files.put(
+        name="install backup script",
+        src=path.join(backup_files_dir, "backup"),
+        dest="/usr/local/bin/backup",
+        user="root",
+        group="root",
+        mode="0555",
+    )
 
-        p.files.directory(
-            name="backup config directory",
-            path=backup_config_dir,
-            user="root",
-            group="root",
-            mode="0555",
-        )
+    p.files.directory(
+        name="backup config directory",
+        path=backup_config_dir,
+        user="root",
+        group="root",
+        mode="0555",
+    )
 
-        p.files.template(
-            name="backup env",
-            src=path.join(backup_files_dir, "env.conf.j2"),
-            dest=path.join(backup_config_dir, "env.conf"),
-            user="root",
-            group="root",
-            mode="0400",
-            quote=quote,
-            aws_default_region=gvars.aws_default_region,
-            aws_access_key_id=aws_access_key_id(),
-            aws_secret_access_key=aws_secret_access_key(),
-        )
+    p.files.template(
+        name="backup env",
+        src=path.join(backup_files_dir, "env.conf.j2"),
+        dest=path.join(backup_config_dir, "env.conf"),
+        user="root",
+        group="root",
+        mode="0400",
+        quote=quote,
+        aws_default_region=gvars.aws_default_region,
+        aws_access_key_id=aws_access_key_id(),
+        aws_secret_access_key=aws_secret_access_key(),
+    )
 
-        p.files.template(
-            name="syncthing config",
-            src=path.join(backup_files_dir, "syncthing.conf.j2"),
-            dest=path.join(backup_config_dir, "syncthing.conf"),
-            user="root",
-            group="root",
-            mode="0444",
-            quote=quote,
-            backup_working_dir=syncthing_container.data_dir,
-            backup_src=vars.syncthing_backup_src,
-            backup_dest=ssm_parameter_value("/prod/backup/rclone_dest"),
-            bw_limit=vars.syncthing_bw_limit,
-        )
+    p.files.template(
+        name="syncthing config",
+        src=path.join(backup_files_dir, "syncthing.conf.j2"),
+        dest=path.join(backup_config_dir, "syncthing.conf"),
+        user="root",
+        group="root",
+        mode="0444",
+        quote=quote,
+        backup_working_dir=syncthing_container.data_dir,
+        backup_src=vars.syncthing_backup_src,
+        backup_dest=ssm_parameter_value("/prod/backup/rclone_dest"),
+        bw_limit=vars.syncthing_bw_limit,
+    )
 
-        backup_service = p.files.put(
-            name="backup service systemd unit",
-            src=path.join(backup_files_dir, "backup.service"),
-            dest=path.join(gvars.systemd_unit_dir, "backup.service"),
-            user="root",
-            group="root",
-            mode="0444",
-        )
+    backup_service = p.files.put(
+        name="backup service systemd unit",
+        src=path.join(backup_files_dir, "backup.service"),
+        dest=path.join(gvars.systemd_unit_dir, "backup.service"),
+        user="root",
+        group="root",
+        mode="0444",
+    )
 
-        backup_timer = p.files.template(
-            name="backup timer systemd unit",
-            src=path.join(backup_files_dir, "backup.timer.j2"),
-            dest=path.join(gvars.systemd_unit_dir, "backup.timer"),
-            user="root",
-            group="root",
-            mode="0444",
-            backup_time=vars.syncthing_backup_time,
-        )
+    backup_timer = p.files.template(
+        name="backup timer systemd unit",
+        src=path.join(backup_files_dir, "backup.timer.j2"),
+        dest=path.join(gvars.systemd_unit_dir, "backup.timer"),
+        user="root",
+        group="root",
+        mode="0444",
+        backup_time=vars.syncthing_backup_time,
+    )
 
-        p.systemd.service(
-            name="start backup timer",
-            service="backup.timer",
-            running=True,
-            enabled=True,
-            restarted=backup_timer.changed,
-            daemon_reload=backup_service.changed or backup_timer.changed,
-        )
+    p.systemd.service(
+        name="start backup timer",
+        service="backup.timer",
+        running=True,
+        enabled=True,
+        restarted=backup_timer.changed,
+        daemon_reload=backup_service.changed or backup_timer.changed,
+    )
 
 
 def provision_service_containers(p: Pyinfra):
