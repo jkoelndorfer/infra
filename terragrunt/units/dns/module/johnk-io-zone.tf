ec2684e19e0cf88f0af24df28129ab4e2ea0f7fd
John Koelndorfer <john@johnk.io>
2025-08-17 20:14:37 -0500

    terragrunt/dns: Configure email DNS records for johnk.io

--- terragrunt/units/dns/module/johnk-io-zone.tf
+++ terragrunt/units/dns/module/johnk-io-zone.tf
@@ -8,3 +8,15 @@ module "johnk_io_zone" {
   function        = "johnk-io"
   project         = module.project
 }
+
+module "johnk_io_email_records" {
+  count  = module.johnk_io_zone.zone_was_created ? 1 : 0
+  source = "${var.paths.modules_root}/google_email_dns_records"
+
+  addl_apex_txt_rrdata = [
+    "google-site-verification=vZjbCaD_Jb_ACR00pLxQm3_ewS6seMRGwK0-Dq8_4ds",
+  ]
+  dkim_rrdata  = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWYN8FrWcBLOWI7C54HM2NZS093g/K6q6QiH09NmXH1BBW+HFatcpJzN9O95ns0oHHV7Tpii8cYFM09qbX+OoxZGOfgQlaSwyLl36UwgPMGTqgkXrbBlCD1mibgyQ+o0T/v1NCL1c3PyUHS5CM3Xur2HoxImb6el74vvfCWRR6fQIDAQAB"
+  dmarc_rrdata = "v=DMARC1;p=none;pct=100;fo=1"
+  zone         = module.johnk_io_zone
+}
