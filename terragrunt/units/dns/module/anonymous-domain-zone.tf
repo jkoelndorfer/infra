ce76cc8c1ec68b7fd3f5d86ab20535c3adf91a52
John Koelndorfer <john@johnk.io>
2025-09-19 19:12:42 -0500

    terragrunt: Implement anonymous_domains module

--- terragrunt/units/dns/module/anonymous-domain-zone.tf
+++ terragrunt/units/dns/module/anonymous-domain-zone.tf
@@ -1,20 +1,13 @@
-data "google_secret_manager_secret_version_access" "anonymous_domains" {
-  project = var.google_infra_mgmt_project.project_id
-
-  secret = "anonymous-domains"
-}
-
-locals {
-  anonymous_domains      = jsondecode(data.google_secret_manager_secret_version_access.anonymous_domains.secret_data)
-  anonymous_domains_keys = nonsensitive([for k, v in local.anonymous_domains : k if k != "//"])
+module "anonymous_domains" {
+  source = "${var.paths.modules_root}/anonymous_domains"
 }
 
 module "anonymous_domain_zone" {
   source   = "${var.paths.modules_root}/google_dns_managed_zone"
-  for_each = toset(local.anonymous_domains_keys)
+  for_each = module.anonymous_domains.domains
 
   description     = nonsensitive("anonymous domain ${each.key}")
-  dns_name        = nonsensitive(local.anonymous_domains[each.key])
+  dns_name        = nonsensitive(each.value)
   enable_dev_zone = false
   env             = var.env
   function        = each.key
