497d1879ac46eab2c432f850a390ca587182fcd7
John Koelndorfer <john@johnk.io>
2025-10-26 12:06:06 -0500

    backupjob: Implement DataGenerator for tests

--- container-images/backupjob/app/tests/testlib/__init__.py
+++ container-images/backupjob/app/tests/testlib/__init__.py
@@ -6,5 +6,6 @@ Contains code to support tests.
 """
 
 from .cmd import CommandResultFactoryOutput, MockCommandExecutor
+from .datagen import DataGenerator
 
-__all__ = ["CommandResultFactoryOutput", "MockCommandExecutor"]
+__all__ = ["CommandResultFactoryOutput", "DataGenerator", "MockCommandExecutor"]
