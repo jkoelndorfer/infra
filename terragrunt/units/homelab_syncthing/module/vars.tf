166006fd5095406b194b5380f0f078811ca7712b
John Koelndorfer <john@koelndorfer.com>
2025-11-17 19:24:00 -0600

    syncthing: Use UID and GID defined in uid_gid module

--- terragrunt/units/homelab_syncthing/module/vars.tf
+++ terragrunt/units/homelab_syncthing/module/vars.tf
@@ -8,15 +8,3 @@ variable "homelab_shared01_zone" {
   description = "the shared01 sub-zone used for homelab services"
   type        = object({ dns_name = string, host = string, name = string, project = string, zone_was_created = bool })
 }
-
-variable "syncthing_gid" {
-  description = "the GID that syncthing runs as"
-  type        = number
-  default     = 911
-}
-
-variable "syncthing_uid" {
-  description = "the UID that syncthing runs as"
-  type        = number
-  default     = 911
-}
