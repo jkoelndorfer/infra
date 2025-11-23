992f37d4abf382c5c2d039146dbda3166f78cf1e
John Koelndorfer <john@koelndorfer.com>
2025-11-23 15:11:30 -0600

    homelab dns: Configure outgoing email via SMTP2GO

---
+++ terragrunt/units/homelab_dns/module/smtp2go.tf
@@ -0,0 +1,23 @@
+resource "google_dns_record_set" "smtp2go_return" {
+  project = module.project.project_id
+
+  name = "em911349.${module.homelab_shared01_zone.dns_name}"
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = module.homelab_shared01_zone.name
+
+  rrdatas = ["return.smtp2go.net."]
+}
+
+resource "google_dns_record_set" "smtp2go_dkim" {
+  project = module.project.project_id
+
+  name = "s911349._domainkey.${module.homelab_shared01_zone.dns_name}"
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = module.homelab_shared01_zone.name
+
+  rrdatas = ["dkim.smtp2go.net."]
+}
