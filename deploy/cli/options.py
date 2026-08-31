2773fed5290a76d5822fff37898a7a6d10ecbb02
John Koelndorfer <john@koelndorfer.com>
2026-08-31 08:28:58 -0500

    pcli: Adjust stack option help text

--- deploy/cli/options.py
+++ deploy/cli/options.py
@@ -13,18 +13,18 @@ environment = option(
     "--environment",
     "-e",
     type=Choice(Environment, case_sensitive=False),
-    help="The environment to deploy to.",
+    help="The target environment.",
     required=True,
 )
 confirm = option(
     "--confirm/--no-confirm", help="Prompt for confirmation.", default=True
 )
 project = option(
-    "--project", "-p", help="The infrastructure project to operate on.", required=True
+    "--project", "-p", help="The target infrastructure project.", required=True
 )
 refresh = option(
     "--refresh/--no-refresh",
     help="Refresh before performing any stack operations.",
     default=True,
 )
-region = option("--region", "-r", help="The cloud region to deploy to.", default=None)
+region = option("--region", "-r", help="The target cloud region.", default=None)
